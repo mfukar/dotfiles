e93888a1a85fb5a5b6743c9097a2374731974eba
Michael Foukarakis <michael.foukarakis@ioactive.com>
2016-02-03 12:24:24 +0200

    Helpers to deal with Android madness

--- .bashrc.java
+++ .bashrc.java
@@ -3,16 +3,17 @@ toggle_jvm()
 {
     current=$(echo $JAVA_HOME | sed "s/^.*\([0-9]\.[0-9]\.[0-9]\).*$/\1/g")
 
+    echo "$current"
     case $current in
-        1.6.0)
+        1.7.0)
+            export JAVA_HOME=$(/usr/libexec/java_home -v "1.8*")
+            ;;
+        1.8.0)
             export JAVA_HOME=$(/usr/libexec/java_home -v "1.7*")
             ;;
-        1.7.0)
-            export JAVA_HOME=$(/usr/libexec/java_home -v "1.6*")
-            ;;
         *)
-            echo "Java version detected [$current], reverting to 1.6.0"
-            export JAVA_HOME=$(/usr/libexec/java_home -v "1.6*")
+            echo "Java version detected [$current], reverting to 1.8*"
+            export JAVA_HOME=$(/usr/libexec/java_home -v "1.8*")
             ;;
     esac
 }

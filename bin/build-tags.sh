9d108c525eb38f28ec8be534a0f10df92ad2db26
Michael Foukarakis <michael.foukarakis@bmw-carit.de>
2018-06-08 10:16:22 +0200

    build-tags: Handle paths with spaces
    Signed-off-by: Michael Foukarakis <michael.foukarakis@bmw-carit.de>

--- bin/build-tags.sh
+++ bin/build-tags.sh
@@ -10,11 +10,11 @@ pushd $CWD &>/dev/null
 
 STARTTIME=$(date +%s)
 echo "Generating tag files.."
-find -P $(pwd) -type f -a \( -name '*.[ch]' -o -name '*.cpp' -o -name '*.hpp' \) > ./cscope.files
+find -P $(pwd) -type f -a \( -name '*.[ch]' -o -name '*.cpp' -o -name '*.hpp' \) -fprintf cscope.files '"%p"\n'
 $CSCOPEBIN -buf $OUT_FNAME
 rm -f ./cscope.files
 echo "Gathering user-defined types.."
-$CTAGSBIN --c-kinds=gstu --c++-kinds=gstu -f- $(find . -iname "*.[ch]" -o -iname "*.[ch]pp") | awk 'BEGIN{printf("syntax keyword udType ")} {printf("%s ", $1)} END{print "\nhi def link udType Type"}' > _ud_types.vim
+find . -iname "*.[ch]" -o -iname "*.[ch]pp" -print0 | xargs -0 $CTAGSBIN --c-kinds=gstu --c++-kinds=gstu -f- | awk 'BEGIN{printf("syntax keyword udType ")} {printf("%s ", $1)} END{print "\nhi def link udType Type"}' > _ud_types.vim
 ENDTIME=$(date +%s)
 
 echo "..done in $(($ENDTIME - $STARTTIME)) seconds. :-)"

407f73a325ccfa5f1a12afc033660a429e872d7a
Michael Foukarakis <michael.foukarakis@bmw-carit.de>
2018-09-21 19:12:07 +0200

    tags: Skip some locations
    Makes tag DBs more bearable in some cases, e.g. Linux kernel.
    
    Signed-off-by: Michael Foukarakis <michael.foukarakis@bmw-carit.de>

--- bin/build-tags.sh
+++ bin/build-tags.sh
@@ -17,11 +17,23 @@ pushd $CWD &>/dev/null
 
 STARTTIME=$(date +%s)
 echo "Generating tag files.."
-$FIND -P $(pwd) -type f -a \( -name '*.[ch]' -o -name '*.cpp' -o -name '*.hpp' \) -fprintf cscope.files '"%p"\n'
-$CSCOPEBIN -buf $OUT_FNAME
+$FIND -P $CWD \
+    \( -path "$CWD/Documentation*" -prune \) -o \
+    \( -path "$CWD/scripts*" -prune \) -o \
+    \( -path "$CWD/drivers*" -prune \) -o \
+    \( -path "$CWD/tools*" -prune \) -o \
+    \( -iname '*.[ch]' -o -iname '*.[ch]pp' \) -a \
+    -type f -fprintf cscope.files '"%p"\n'
+# $FIND -P $(pwd) -type f -a \( -name '*.[ch]' -o -name '*.cpp' -o -name '*.hpp' \) -fprintf cscope.files '"%p"\n'
+$CSCOPEBIN -bquf $OUT_FNAME
 rm -f ./cscope.files
 echo "Gathering user-defined types.."
-find . \( -iname "*.[ch]" -o -iname "*.[ch]pp" \) -print0 | xargs -0 $CTAGSBIN --c-kinds=gstu --c++-kinds=gstu -f- | awk 'BEGIN{printf("syntax keyword udType ")} {printf("%s ", $1)} END{print "\nhi def link udType Type"}' > _ud_types.vim
+find . \( -path "./Documentation*" -prune \) -o \
+       \( -path "./scripts*" -prune \) -o \
+       \( -path "./drivers*" -prune \) -o \
+       \( -iname "*.[ch]" -o -iname "*.[ch]pp" \) -print0 \
+ | xargs -0 $CTAGSBIN --c-kinds=gstu --c++-kinds=gstu -f- \
+ | awk 'BEGIN{printf("syntax keyword udType ")} {printf("%s ", $1)} END{print "\nhi def link udType Type"}' > _ud_types.vim
 ENDTIME=$(date +%s)
 
 echo "..done in $(($ENDTIME - $STARTTIME)) seconds. :-)"
